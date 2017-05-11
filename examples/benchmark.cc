53bb377dce6f3a6c32b06bff68bfb547a3e0f6fc
Qiming Lu <qlu@fnal.gov>
2017-05-11 12:54:33 -0500

    benchmark executable

--- examples/benchmark.cc
+++ examples/benchmark.cc
@@ -2,31 +2,19 @@
 #include <stdexcept>
 
 #include "synergia/lattice/lattice.h"
-#include "synergia/utils/serialization.h"
 #include "synergia/simulation/operator.h"
 #include "synergia/simulation/lattice_simulator.h"
-#include "synergia/simulation/split_operator_stepper.h"
 #include "synergia/simulation/propagator.h"
 #include "synergia/bunch/bunch.h"
-#include "synergia/foundation/distribution.h"
-#include "synergia/bunch/populate.h"
-#include "synergia/bunch/diagnostics_basic.h"
-#include "synergia/bunch/diagnostics_full2.h"
-#include "synergia/collective/space_charge_3d_open_hockney.h"
 
-#include "benchmark_options.h"
 
 // We put the actual code in a separate function so that shared_ptr's can
 // be cleanup up properly before we call MPI_Finalize.
-void
-run(Benchmark_options const& opts)
+void run()
 {
-    std::vector<int > grid_shape(3);
-    grid_shape[0] = opts.gridx;
-    grid_shape[1] = opts.gridy;
-    grid_shape[2] = opts.gridz;
+    std::vector<int > grid_shape = { 16, 16, 16 };
 
-    const int part_per_cell = opts.partpercell;
+    const int part_per_cell = 10;
     const int num_macro_particles = grid_shape[0] * grid_shape[1] * grid_shape[2] * part_per_cell;
 
     const int seed = 4;
@@ -36,12 +24,16 @@ run(Benchmark_options const& opts)
     const int num_turns = 4;
     const int map_order = 2;
 
+    int verbosity = 6;
+
 #if 0
     Lattice_sptr lattice_sptr(new Lattice());
     lattice_sptr->set_all_string_attribute("extractor_type","chef_propagate");
 #endif
 
-    Lattice lattice();
+    Commxx_sptr comm = std::make_shared<Commxx>();
+
+    Lattice lattice;
     Bunch bunch(lattice.get_reference_particle(), num_macro_particles, num_real_particles, comm);
 
 #if 0
@@ -60,8 +52,9 @@ run(Benchmark_options const& opts)
     Space_charge_3d_open_hockney_sptr space_charge_sptr(new Space_charge_3d_open_hockney(grid_shape));
 #endif
 
-    Space_charge_3d_open_hockney space_charge();
-    Stepper stepper(lattice, space_charge, num_steps);
+    //Space_charge_3d_open_hockney space_charge();
+    Dummy_collective_operator space_charge("dummy");
+    Stepper stepper(lattice, map_order, space_charge, num_steps);
 
 #if 0
     Lattice_simulator lattice_simulator(lattice_sptr, map_order);
@@ -71,12 +64,12 @@ run(Benchmark_options const& opts)
 #endif
 
     Propagator propagator(stepper);
-    Bunch_simulator bunch_simulator(bunch);
+    Simulator simulator(bunch);
 
     double t0 = MPI_Wtime();
 
     const int max_turns = 0;
-    propagator.propagate(bunch_simulator, num_turns, max_turns, opts.verbosity);
+    propagator.propagate(simulator, num_turns, max_turns, verbosity);
 
     double t1 = MPI_Wtime();
     std::cout << "propagate time = " << (t1 - t0) << std::endl;
@@ -87,8 +80,7 @@ main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    Benchmark_options opts(argc, argv);
-    run(opts);
+    run();
 
     MPI_Finalize();
     return 0;
